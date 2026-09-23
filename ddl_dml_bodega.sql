01cdb0b29f5102668cdd410853f568b3f40a26b0
goldscheidtc-beep <goldscheidtc@gmail.com>
2026-09-22 23:59:16 -0300

    Update ddl_dml_bodega.sql
    Correcciones finales en ddl_dml_bodega.sql según feedback

--- ddl_dml_bodega.sql
+++ ddl_dml_bodega.sql
@@ -1,10 +1,8 @@
--- ________________________________________________
 -- BodegaTech – Script de Inventario
 -- Autor: Carolina Goldscheidt
--- Fecha: 21/09/2026
--- ________________________________________________
+-- Fecha: 22/09/2026
 
--- SECCIÓN DDL _________________________________
+-- SECCIÓN DDL 
 DROP TABLE IF EXISTS inventario;
 
 CREATE TABLE inventario (          
@@ -15,42 +13,42 @@ precio_unitario DECIMAL(10,2),      -- 	Precio de venta en USD
 stock_actual INT,                   -- Unidades disponibles
 stock_minimo INT,                   -- 	Umbral mínimo de reposición
 fecha_ingreso DATE ,                -- 	Fecha de ingreso al inventario
-activo TINYINT(1)                   -- 1 = disponible, 0 = descontinuado
+activo BIT                          -- 1 = disponible, 0 = descontinuado
 );
 
-- SECCIÓN DML _________________________________
+-- SECCIÓN DML 
 INSERT INTO inventario (id_producto, nombre_producto, categoria, precio_unitario, stock_actual, stock_minimo, fecha_ingreso, activo)
 VALUES
 (1, 'Laptop Pro 15', 'Computación',	1200.00, 15, 3,'2024-01-10', 1),
 (2, 'Mouse Inalámbrico', 'Accesorios', 28.00, 80, 10, '2024-01-10', 1),
 (3, 'Monitor 4K 27"', 'Computación', 450.00, 12, 2, '2024-01-15', 1),
-(4, 'Teclado Mecánico', 'Accesorios',	95.00	40,	5, '2024-01-15', 1),
+(4, 'Teclado Mecánico', 'Accesorios', 95.00, 40,	5, '2024-01-15', 1),
 (5,	'Laptop Basic 14',	'Computación',	650.00,	20,	3,	'2024-02-01',	1),
 (6,	'Auriculares BT Pro',	'Audio',	120.00,	35,	5,	'2024-02-01',	1),
-(7,	'Hub USB-C 7 puertos',	'Accesorios',	45.00	60,	10,	'2024-02-10',	1),
+(7,	'Hub USB-C 7 puertos',	'Accesorios',	45.00, 60,	10,	'2024-02-10',	1),
 (8,	'Webcam HD 1080p',	'Accesorios',	85.00,	25,	5,	'2024-02-10',	1),
-(9,	'SSD Externo 1TB',	'Almacenamiento',	130.00,	18,	,3,	'2024-03-01',	1),
-(10,	'Parlante Bluetooth',	'Audio',	60.00	45,	8,	'2024-03-01',	1)
-);
+(9,	'SSD Externo 1TB', 'Almacenamiento', 130.00, 18, 3,	'2024-03-01', 1),
+(10,	'Parlante Bluetooth',	'Audio',	60.00,	45,	8,	'2024-03-01',	1)
+;
 
--- SECCIÓN DML   
--- UPDATE ventas del día
+-- SECCIÓN DML
+UPDATE inventario
 SET stock_actual = stock_actual - 3 
 WHERE id_producto = 1; -- Laptop Pro 15
 
--- UPDATE ventas del día
+UPDATE inventario
 SET stock_actual = stock_actual - 12 
 WHERE id_producto = 2; -- Mouse Inalámbrico
 
-UPDATE ventas del día
+UPDATE inventario
 SET stock_actual = stock_actual - 5 
 WHERE id_producto = 6; -- Auriculares BT Pro
 
-UPDATE producto descontinuado
+-- Desactivar Webcam
+UPDATE inventario
 SET activo = 0 
 WHERE id_producto = 8; -- Webcam HD 1080p
 
--- SELECT validaciones
--- Ver la tabla completa para confirmar que los datos se cargaron
+-- Validacion final
 SELECT * FROM inventario;
 
